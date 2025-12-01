d5f816ac091de392981c3b85b61ff2bfb601cbf5
Jay Prajapati <jay.prajapati5717@gmail.com>
2025-12-01 21:53:29 +0530

    Feat: add tools to mcp server

--- mcp_server.py
+++ mcp_server.py
@@ -1,5 +1,5 @@
 from mcp.server.fastmcp import FastMCP
-
+from pydantic import Field
 mcp = FastMCP("DocumentMCP", log_level="ERROR")
 
 
@@ -13,7 +13,30 @@ docs = {
 }
 
 # TODO: Write a tool to read a doc
+@mcp.tool(
+    name="read_doc_content",
+    description="Read the contents of a document and return it as a string."
+)
+def read_document(
+    doc_id: str = Field(description="Id of the document to read")
+):
+    if doc_id not in docs:
+        raise ValueError(f"Doc with id {doc_id} not found")
+    return docs[doc_id]
+
 # TODO: Write a tool to edit a doc
+@mcp.tool(
+    name= "edit_doc_content",
+    description="Edit a document by replacing a string in the documents content with a new string"
+)
+def edit_document(
+    doc_id: str = Field(description="Id of a document that will be edited"),
+    old_str: str = Field(description="The text to replace. Must match exactly, including the whitespaces"),
+    new_str: str = Field(description="The new text to insert in place of the old text")
+):
+    if doc_id not in docs:
+        raise ValueError(f"doc with id {doc_id} not found")
+    docs[doc_id] = docs[doc_id].replace(old_str, new_str)
 # TODO: Write a resource to return all doc id's
 # TODO: Write a resource to return the contents of a particular doc
 # TODO: Write a prompt to rewrite a doc in markdown format
